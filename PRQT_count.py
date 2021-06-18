634757562bcea1613ac914047e2ac0e3bfff5adc
EmiliaKP <86105256+EmiliaKP@users.noreply.github.com>
2021-06-18 12:37:28 +0200

    Create PRQT_count.py

---
+++ PRQT_count.py
@@ -0,0 +1,13 @@
+#%% powołanie sesji Apache Spark
+from pyspark.sql import SparkSession
+from pyspark.storagelevel import StorageLevel
+
+spark = SparkSession.builder.master("local[*]").appName("PRQT_count").getOrCreate()
+
+#%% wczytanie danych
+from pyspark.sql.functions import col
+
+df=spark.read.parquet("/home/ubuntu/spark-3.0.1-bin-hadoop2.7/source_p/ghtorrent-2019-01-07.parquet")
+
+print(df.count())
+print(df.filter(col('comment_id')>270000000).count())
