1f3a927cbe423ec270946f43eb7f4ddfda4cf5f1
EmiliaKP <86105256+EmiliaKP@users.noreply.github.com>
2021-06-18 12:38:57 +0200

    Create CSV_join_shuffle_480.py

---
+++ CSV_join_shuffle_480.py
@@ -0,0 +1,23 @@
+#%% powołanie sesji Apache Spark
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.master("local[*]").appName("CSV_join_shuffle_480").getOrCreate()
+
+#%% ustawienie parametrow Sparka
+spark.conf.set("spark.sql.shuffle.partitions",480)
+
+#%% wczytanie danych
+df_maly=spark.read.options(header='true',delimiter=',', inferSchema='true')\
+    .csv("/home/ubuntu/spark-3.0.1-bin-hadoop2.7/source_broadcast")
+
+df_wielki=spark.read.options(header='true',delimiter=',', inferSchema='true')\
+    .csv("/home/ubuntu/spark-3.0.1-bin-hadoop2.7/source_ALL")
+    
+
+#%% połączenie tabel względem kolumny comment_id
+df_join=df_wielki.join(df_maly, df_wielki.comment_id==df_maly.comment_id_broad ,'left')
+
+#%% zapis do pliku
+print("Kalkulacja i wczytywanie do plików...")
+df_join.write.mode("overwrite").csv("/home/ubuntu/spark-3.0.1-bin-hadoop2.7/outputs/CSV_join.csv", header=True)
+print("Dane zapisane do plików.")
